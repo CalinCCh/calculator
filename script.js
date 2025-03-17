ab7c5b0c0a85054a108152703f333eeac293ac6e
CalinCCh <calinxirila@gmail.com>
2025-03-17 17:41:26 +0000

    Functionality done, design and some bug fixes needs to be done

---
+++ script.js
@@ -0,0 +1,110 @@
+let inputValue = document.querySelector(".input");
+
+const buttons = document.querySelectorAll("button");
+
+let num1, num2, operator;
+
+let isNum2 = false; //False -> num1 | True -> num2
+
+let numEntered = false; //Prevents from doing another operation until another number is entered
+
+let operatorActive = false;
+let numberAtive = false;
+
+function setDefault() {
+  inputValue.textContent = "0";
+  num1 = null;
+  num2 = null;
+  operator = null;
+  isNum2 = false;
+  operatorActive = false;
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    switch (button.textContent) {
+      case "AC":
+        setDefault();
+        return;
+      case "+":
+      case "-":
+      case "*":
+      case "/":
+        if (!isNum2) {
+          num1 = inputValue.textContent;
+          isNum2 = true;
+        } else {
+          num2 = inputValue.textContent;
+          isNum2 = false;
+          if (numEntered) {
+            inputValue.textContent = operate(num1, num2, operator);
+            num1 = inputValue.textContent;
+            numEntered = false;
+          }
+        }
+        operatorActive = true;
+        operator = button.textContent;
+        return;
+      case "=":
+        num2 = inputValue.textContent;
+        if (num1 !== null && num2 !== null && operator !== null) {
+          inputValue.textContent = operate(num1, num2, operator);
+        }
+        return;
+      case "DEL":
+        if (inputValue.textContent.length === 1) {
+          inputValue.textContent = "0";
+        } else {
+          inputValue.textContent = inputValue.textContent.slice(0, -1);
+        }
+        return;
+      case "+/-":
+        if (inputValue.textContent !== "0")
+          if (inputValue.textContent.slice(0, 1) === "-") {
+            inputValue.textContent = inputValue.textContent.substring(1);
+          } else {
+            inputValue.textContent = "-" + inputValue.textContent;
+          }
+        return;
+      case ".":
+        if (!inputValue.textContent.includes(".")) {
+          inputValue.textContent += button.textContent;
+        }
+        return;
+
+      default:
+        numEntered = true;
+        if (inputValue.textContent === "0") {
+          inputValue.textContent = "";
+        }
+
+        if (operatorActive) {
+          inputValue.textContent = "";
+          operatorActive = false;
+        }
+
+        inputValue.textContent += button.textContent;
+    }
+  });
+});
+
+function operate(num1, num2, operator) {
+  num1 = Number(num1);
+  num2 = Number(num2);
+
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    case "/":
+      if (num2 === 0) return "manin🤘";
+      return num1 / num2;
+    default:
+      return "Invalid operator";
+  }
+}
+
+setDefault();
